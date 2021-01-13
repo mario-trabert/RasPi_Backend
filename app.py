a3c2aa1e5efdb1357c58324ef0965d8510ca6bbd
Roman Oechslin <roman.oechslin@accenture.com>
2021-01-13 15:44:39 +0100

    add blink to app.py and add testing html endpoints

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_from_directory, session, jsonify
 from flask_session import Session
-from gpiozero import Button,LED
+from gpiozero import Button, LED
 from time import sleep
 
 '''
@@ -52,7 +52,7 @@ state_led = LED(24)
 #set the magnet off; remember the magnet works with inverted logic
 magnet.on()
 state_led.on()
-print("here")
+print("Script app.py has started")
 
 def blink(times=1, length=0.25):
     for i in range(times):
@@ -61,6 +61,19 @@ def blink(times=1, length=0.25):
         sleep(length)
         state_led.on()
         sleep(length)
+
+
+@app.route('/test_bill')
+def test_bill_template():
+    return render_template("bill.html", quantity=42)
+
+@app.route('/test')
+def test_template():
+    return render_template("test.html")
+
+@app.route('/test_index')
+def test_index_template():
+    return render_template('index.html', magnet_state=session["status_magnet"], door_state=session["status_door"])
 
 @app.route('/get_state')
 def get_state():
@@ -85,7 +98,7 @@ def set_state0():
 
     return (str(session['status_session']) + " set")
 
-# qr-code wwas detected
+# qr-code was detected
 # fridge is unlocked, but closed
 # Endpoint will be called automatically when QR-Code detected
 @app.route('/set_state1')
@@ -95,10 +108,11 @@ def set_state1():
     session["status_door"] = "closed"
 
     magnet.off()
+    
     return render_template('index.html', magnet_state=session["status_magnet"], door_state=session["status_door"])
     #return (str(session['status_session']) + " set")
 
-# fridge wurde geoeffnet
+# fridge was opened
 @app.route('/set_state2')
 def set_state2():
     session["status_session"] = "state2"
@@ -108,7 +122,7 @@ def set_state2():
     return render_template('index.html', magnet_state=session["status_magnet"], door_state=session["status_door"])
     #return (str(session['status_session']) + " set")
 
-# fridge wurde geschlossen, bill soll angezeigt werden
+# fridge was closed, bill shall be shown
 @app.route('/set_state3')
 def set_state3():
     session["status_session"] = "state3"
@@ -119,14 +133,14 @@ def set_state3():
     # bill_show()
 
     #return render_template('index.html', magnet_state=session["status_magnet"], door_state=session["status_door"])
-    return render_template('index.html', magnet_state=session["status_magnet"], door_state="Ende Gelaende")
-    #return (str(session['status_session']) + " set")
+    #return render_template('index.html', magnet_state=session["status_magnet"], door_state="Ende Gelaende")
+    return (str(session['status_session']) + " set")
 
 # function will be called continuously
 # status will be updated according to measured state
 @app.route('/update-state_fridge')
 def control_update_fridge_state():
-    session["status_door"] = "closed"
+    #session["status_door"] = "closed"
     # door_sensor.value == 1 --> closed
     # door_sensor.value == 0 --> open
     # door_sensor.value
@@ -140,6 +154,10 @@ def control_update_fridge_state():
         set_state0()
         return("status is not set. set state0")
 
+    # if last_status wasn't set yet
+    if str(session.get('last_status')) == "None":
+        session["last_status"] = "state0"
+
     # if state=state1 and status_door = "open", set state2
     elif str(session.get('status_session')) == "state1" and session["status_door"] == "open":
         blink(times=2)
@@ -152,6 +170,7 @@ def control_update_fridge_state():
 
     # return render_template('index.html', magnet_state=session["status_magnet"], door_state=session["status_door"])
     if str(session.get('status_session')) == "state3":
+        print("this is state 3")
         #return("This is a bill")
         return render_template('bill.html', quantity=42)
     else:
